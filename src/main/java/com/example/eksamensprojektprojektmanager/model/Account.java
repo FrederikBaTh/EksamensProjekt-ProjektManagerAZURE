9b3d44428ab212fa945e23d6f5f0539a4a2fb2f9
PaulSebastianKrus <paulsebastiankrus@gmail.com>
2024-05-08 11:08:32 +0200

    subproject

--- src/main/java/com/example/eksamensprojektprojektmanager/model/Account.java
+++ src/main/java/com/example/eksamensprojektprojektmanager/model/Account.java
@@ -7,16 +7,17 @@ public class Account {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private int user_id;
 
-    @Column(nullable = false, unique = true)
+    @Column(name = "username",nullable = false, unique = true)
     private String username;
 
-    @Column(nullable = false)
+    @Column(name = "password",nullable = false)
     private String password;
 
 
-    @Column(nullable = false,name = "is_admin")
+    @Column(name = "is_admin",nullable = false)
     private boolean isAdmin;
 
 
