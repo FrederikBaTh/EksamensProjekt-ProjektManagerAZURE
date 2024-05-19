a9c33e4d034d4f7eefafb44281a99411329251b1
SoerenTP <sope0001@stud.kea.dk>
2024-05-20 00:27:29 +0200

    Tilføjet CSS Styling til editAccount, myAccount og UserList

--- src/test/java/com/example/eksamensprojektprojektmanager/AccountTest/AccountControllerTest.java
+++ src/test/java/com/example/eksamensprojektprojektmanager/AccountTest/AccountControllerTest.java
@@ -1,21 +1,18 @@
-package com.example.eksamensprojektprojektmanager;
+package com.example.eksamensprojektprojektmanager.AccountTest;
 
 import com.example.eksamensprojektprojektmanager.model.Account;
 import com.example.eksamensprojektprojektmanager.service.AccountService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -52,4 +49,6 @@ public class AccountControllerTest {
 
         verify(accountService).saveAccount(any(Account.class));
     }
+
+
 }
--- src/test/java/com/example/eksamensprojektprojektmanager/AccountTest/AccountRepositoryTest.java
+++ src/test/java/com/example/eksamensprojektprojektmanager/AccountTest/AccountRepositoryTest.java
@@ -1,21 +1,13 @@
-package com.example.eksamensprojektprojektmanager;
+package com.example.eksamensprojektprojektmanager.AccountTest;
 
-import com.example.eksamensprojektprojektmanager.model.Account;
 import com.example.eksamensprojektprojektmanager.repository.AccountRepository;
 import com.example.eksamensprojektprojektmanager.service.AccountService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementSetter;
-import org.springframework.jdbc.core.RowMapper;
-
-import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
