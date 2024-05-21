9d77a520380fab15c9769a6ba8e1174bb3b2c303
PaulSebastianKrus <paulsebastiankrus@gmail.com>
2024-05-21 15:05:36 +0200

    test account

---
+++ src/test/java/com/example/eksamensprojektprojektmanager/test/h2Test/AccountRepositoryH2Test.java
@@ -0,0 +1,30 @@
+package com.example.eksamensprojektprojektmanager.test.h2Test;
+
+
+import com.example.eksamensprojektprojektmanager.model.Account;
+import com.example.eksamensprojektprojektmanager.repository.AccountRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@ActiveProfiles("h2")
+public class AccountRepositoryH2Test {
+
+
+    @Autowired
+    AccountRepository repository;
+
+
+    @Test
+    void findAccount(){
+        Account found = repository.getUserById(1L);
+        assertEquals("testUser1",found.getName());
+    }
+
+
+
+}
